fd0b813da1ccb574de925773649cff30eaaf8adc
yemiOdetola <yemiOdetola>
2018-06-22 18:38:54 +0100

    Firebase setup: Trainings from firebase

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
@@ -11,11 +13,17 @@ import { Exercise } from '../exercise.model';
 })
 export class NewTrainingComponent implements OnInit {
 
-  exercises: Exercise[] = [];
-  constructor(private trainingService: TrainingService) { }
+  exercises: Observable<any>;
+
+  constructor(
+      private trainingService: TrainingService,
+      private db: AngularFirestore) { }
 
   ngOnInit() {
-    this.exercises = this.trainingService.getAvailableExercises();
+    this.exercises = this.db
+      .collection('availableExercises')
+      .valueChanges();
+    // this.exercises = this.trainingService.getAvailableExercises();
   }
 
   onStartTraining(form: NgForm) {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB08thXD_DDpkBuuseAc8wiFACzy1_2XdM',
+    authDomain: 'fitness-trainer-ng-35f28.firebaseapp.com',
+    databaseURL: 'https://fitness-trainer-ng-35f28.firebaseio.com',
+    projectId: 'fitness-trainer-ng-35f28',
+    storageBucket: 'fitness-trainer-ng-35f28.appspot.com',
+    messagingSenderId: '391221190368'
+  }
 };
